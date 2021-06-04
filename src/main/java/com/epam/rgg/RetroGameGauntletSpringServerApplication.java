9b1cbf9ce990be0c3ea436410b78b2601b5e6981
Diachenko, Oleksandr <oleksandr.diachenko@epam.com>
2021-06-04 11:58:21 +0300

    Created project

---
+++ src/main/java/com/epam/rgg/RetroGameGauntletSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.epam.rgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroGameGauntletSpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetroGameGauntletSpringServerApplication.class, args);
+    }
+
+}
