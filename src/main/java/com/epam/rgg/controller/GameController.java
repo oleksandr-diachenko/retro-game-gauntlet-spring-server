6c8115b4ffbbad8d6936efe63e5254e08f3c5236
Diachenko, Oleksandr <oleksandr.diachenko@epam.com>
2021-06-10 15:59:41 +0300

    Added endpoint for retrieving random game by console type

--- src/main/java/com/epam/rgg/controller/GameController.java
+++ src/main/java/com/epam/rgg/controller/GameController.java
@@ -3,7 +3,13 @@ package com.epam.rgg.controller;
 import com.epam.rgg.dto.GameDto;
 import com.epam.rgg.model.ConsoleType;
 import com.epam.rgg.service.GameService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("v1/games")
 public class GameController {
 
     private final GameService gameService;
@@ -12,7 +18,8 @@ public class GameController {
         this.gameService = gameService;
     }
 
-    public GameDto getRandomGame(ConsoleType consoleType) {
+    @GetMapping("/random/{consoleType}")
+    public GameDto getRandomGame(@PathVariable ConsoleType consoleType) {
         return gameService.rollGame(consoleType);
     }
 }
